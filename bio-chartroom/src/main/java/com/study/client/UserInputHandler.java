d436784ceae1c506785f34b89fdb192db3e19502
sorozy <sorozy@163.com>
2020-11-03 20:30:59 +0800

    add

---
+++ bio-chartroom/src/main/java/com/study/client/UserInputHandler.java
@@ -0,0 +1,46 @@
+package com.study.client;
+
+import com.study.server.ChatServer;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Calendar;
+
+
+
+/**
+ * @author 李国栋
+ * @create 2020-11-02 9:41 下午
+ */
+public class UserInputHandler implements Runnable {
+
+    private ChatClient chatClient;
+
+    public UserInputHandler(ChatClient chatClient) {
+        this.chatClient = chatClient;
+    }
+
+    @Override
+    public void run() {
+        try {
+            // 等待用户输入消息
+            BufferedReader consoleReader =
+                    new BufferedReader(new InputStreamReader(System.in));
+            while (true) {
+                String input = consoleReader.readLine();
+
+                // 向服务器发送消息
+                chatClient.send(input);
+
+                // 检查用户是否准备退出
+                if (chatClient.readyToQuit(input)) {
+                    break;
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
